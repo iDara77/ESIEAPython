18a67b2f480ceab4a9d8764ddc8c821aea1641d1
NH <nadim@ob-soft.com>
2023-05-24 11:34:15 +0000

    Added Chiffre Cesar

--- 2_2_substitution.py
+++ 2_2_substitution.py
@@ -1,3 +1,16 @@
+def printSubstitutionResult(result):
+    col=4
+    header = "{}{}{}{}".format("L".center(col),"I".center(col),"J".center(col),"F".center(col))
+    print(header)
+
+    print("-"*len(header))
+    
+    for step in result["steps"]:
+        print(f"{step[0].center(col,'.')}{str(step[1]).center(col,'.')}{str(step[2]).center(col,'.')}{step[3].center(col,'.')}")
+    print("-"*len(header))
+    print(result['word'])
+    print(result['encrypted'])
+    
 # 2.2.1 Chiffre Atbash
 def chiffreAtbash(word):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -15,3 +28,26 @@ result = chiffreAtbash("ESIEA")
 print(" Chiffre Atbash ".center(90,"*"))
 print(result['word'])
 print(result['encrypted'])
+
+# 2.2.2 Chiffre César
+def chiffreCesar(word, dec):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    word = word.lower()
+    encrypted = ""
+    steps = []
+    for letter in word:
+        i = alphabet.index(letter)
+        j = (i + dec) % len(alphabet)
+        encrypted += alphabet[j]
+        steps.append((letter, i, j, alphabet[j]))
+    
+    return {"word": word, "encrypted": encrypted, "steps": steps}
+
+result = chiffreCesar("ESIEA", 10)
+print(" Chiffre Cesar ".center(90,"*"))
+printSubstitutionResult(result)
+
+print("*"*90)
+
+
+
