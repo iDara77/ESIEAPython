00cfbf9fbf753d25cb6ad407f076ffc026171e2a
NH <nadim@ob-soft.com>
2023-06-06 09:13:47 +0000

    Added col mani examples

--- 3_3_pandas_sales.py
+++ 3_3_pandas_sales.py
@@ -10,3 +10,7 @@ print(revenue)
 calls_revenue = pd.merge(calls, revenue, on=['Territory', 'Month'])
 print(calls_revenue)
 print(calls_revenue[calls_revenue.Month == 1])
+print(calls_revenue[calls_revenue.Calls>100])
+print(calls_revenue[calls_revenue.Amount/calls_revenue.Calls>500])
+calls_revenue['Call_Amount'] = calls_revenue.Amount/calls_revenue.Calls
+print(calls_revenue)
