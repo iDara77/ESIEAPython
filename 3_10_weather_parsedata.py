d6bf44af4123af84ecb2198a996f7f3493e6508d
NH <nadim@ob-soft.com>
2023-06-06 13:23:21 +0000

    Filtering data for desired location and values

--- 3_10_weather_parsedata.py
+++ 3_10_weather_parsedata.py
@@ -12,5 +12,12 @@ Inventory = namedtuple("Inventory", ['station', 'latitude', 'longitude',
 inventory = [Inventory(x[0:11], float(x[12:20]), float(x[21:30]), x[31:35],
      int(x[36:40]), int(x[41:45])) for x in inventory_txt.split("\n") if x.startswith("US")]
 
+inventory_temps = [x for x in inventory if x.element in ['TMIN', 'TMAX']
+                   and x.end >= 2015 and x.start < 1920]
+
+latitude, longitude = 41.882, -87.629
+
+inventory_temps.sort(key=lambda x: abs(latitude-x.latitude) + abs(longitude- x.longitude))
+
 for line in inventory[:5]:
     print(line)
